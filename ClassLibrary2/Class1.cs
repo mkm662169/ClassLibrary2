1d981afcd1d19c7254d9d169a16aa0f6b21f54e0
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-09 16:25:21 +0200

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public void something() { }
+    }
+}
---
+++ ClassLibrary2Tests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary2.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void somethingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
